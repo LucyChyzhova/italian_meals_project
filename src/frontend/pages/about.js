b2764beca5222aa9417c7a239b08c8e73fee5064
Lucy Chyzhova <liudmyla.chyzhova@gmail.com>
2020-07-15 13:12:38 +0200

    fixed css About page

---
+++ src/frontend/pages/about.js
@@ -0,0 +1,31 @@
+window.handleAboutRequest = () => {
+    document.body.innerHTML = `
+    <div class="conteiner-about">
+      <div class="about-text">
+          <div><h1>Hi!</h1></div>
+          <div><h2>It was so tricky to get this page. So let's start, this page about me. </h2></div>
+        
+         <div>
+            <p>I like what I'm doing now. It's all about design and coding.
+             I feel so powerful, just think, I started from scratch and then this full backend and frontend stuff. 
+             I don't want to list all programs and languages... BUT I will try it. 
+             Here I used HTML, CSS, JavaScript, MySQL, PostMan, and Node.js. I guess, there is a room for improvement, but I just started :) </p>
+             <h3>Let's make projects together!</h3>
+             <h3>Let's make projects or just drink a cup of coffee together!</h3>
+         </div>
+         
+         <div class="social-net">
+            <div><a class="about-link" href="https://github.com/LucyChyzhova" target="_blank">Github</a></div>          
+            <div><a class="about-link" href="https://www.linkedin.com/in/liudmylachyzhova/" target="_blank">LinkedIn</a></div>
+         </div>
+
+         <div class="about-link go-project-link"><p><a href="/" target="_blank">GO to an Italian meal project...</a></p></div>
+         
+      </div>
+        
+      <div><img class="picture-about" src="https://live.staticflickr.com/65535/50097908823_d32062acd9.jpg" alt="Lucy Chyzhova img"></div>
+        
+        
+    </div> `
+
+}; 
